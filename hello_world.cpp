230c51d935f461a85e48ee55be6794417d3a1232
Magma-Sz <maniaszymon2009@gmail.com>
2025-01-16 15:26:35 +0100

    This commit underlines the start of a new creator
    Just look at the title.

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+﻿// Including iostream
+#include <iostream>
+
+// Main function is entry point of the program
+int main()
+{
+	// This prints hello world
+	std::cout << "hello world";
+	// This returns main with exit code 0
+	return 0;
+}
+
+
